720ac4a52fe2615bddbf64ca0770ff124d0c7030
kha73-code <karen74gh@gmail.com>
2025-03-19 18:53:07 -0600

    Add files via upload

---
+++ Arreglos .cxx
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (){
+
+int miArreglo [8];
+
+miArreglo [0] = 5;
+
+miArreglo [1] = 10;
+
+miArreglo[2] = miArreglo[0]+ miArreglo [1],
+
+system("Pause");
+
+return 0;
+
+} // Fin main
